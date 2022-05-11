f59a9ba6088bdc1e9433796807a7ff5718f8fd28
mzaruna <aruna@mezzofy.com>
2022-05-11 14:54:35 +0530

    Outlet

--- app/src/main/java/com/mezzofy/mzcustomercoupon/MainActivity.java
+++ app/src/main/java/com/mezzofy/mzcustomercoupon/MainActivity.java
@@ -6,8 +6,13 @@ import android.os.Bundle;
 import android.util.Log;
 
 
+import com.mezzofy.mzcustomercouponlib.Data.CustomerData;
 import com.mezzofy.mzcustomercouponlib.Data.LoginRequest;
 import com.mezzofy.mzcustomercouponlib.Data.LoginResponse;
+import com.mezzofy.mzcustomercouponlib.Data.MerchantDownloadSettings;
+import com.mezzofy.mzcustomercouponlib.Data.MerchantSettingModel;
+import com.mezzofy.mzcustomercouponlib.Data.OutletProfile;
+import com.mezzofy.mzcustomercouponlib.Data.OutletProfileModel;
 import com.mezzofy.mzcustomercouponlib.Data.SerialResponse;
 import com.mezzofy.mzcustomercouponlib.RemoteUtils.mzApiClient;
 
@@ -27,8 +32,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 //        login();
 //        SerialList();
-        SerialListbyCouponId();
-        SerialListHistorybyCustomerId();
+//        SerialListbyCouponId();
+//        SerialListHistorybyCustomerId();
+//        getCustomerDetail();
+//        getMerchantSetting();
+//        getMerchantDownloadSetting();
+//        getOutletByCouponCode();
+        getOutletByRedeemPass();
     }
 
     public void login(){
@@ -58,7 +68,8 @@ public class MainActivity extends AppCompatActivity {
         params.put("skip", "0");
         params.put("status", "R");
 
-        Call<SerialResponse> serialResponseCall= mzApiClient.getSerialServices("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Ik9MVE1GMyIsInBhc3N3b3JkIjoiOHF5dSIsImlhdCI6MTY1MTgyNzIzOSwiZXhwIjoxNjUxODU2MDM5fQ.DO1FofsDm8iAZKxj1GMZ33qCJC9cK5CqrMufM-n2KSE").getSerialResponseByCustomerId("PNCEUL",params);
+        Call<SerialResponse> serialResponseCall= mzApiClient.getSerialServices("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Ik9MVE1GMyIsInBhc3N3b3JkIjoiOHF5dSIsImlhdCI6MTY1MTg0MDI0MywiZXhwIjoxNjUxODY5MDQzfQ.HYVkQ4hqLpAmlOW_DgAMYwnSCBiGcb8eHYeEyZj2HYU").getSerialResponseByCustomerId("PNCEUL",params);
+
         serialResponseCall.enqueue(new Callback<SerialResponse>() {
             @Override
             public void onResponse(Call<SerialResponse> call, Response<SerialResponse> response) {
@@ -112,4 +123,92 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    public void getCustomerDetail(){
+
+
+        Call<CustomerData> customerDataCall= mzApiClient.getCustomerServices("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkhEQ0s1UkhCN0ZGSldUV1ciLCJwYXNzd29yZCI6IjlRN1UiLCJpYXQiOjE2NTIxNjA2MDQsImV4cCI6MTY1MjE4OTQwNH0.GSfwYKNMRTlg8pyIZSuts8UgmbrlDlC7cYsbo0mzpXE").getCustomerDetailByCustomerId("8PX3GI");
+        customerDataCall.enqueue(new Callback<CustomerData>() {
+            @Override
+            public void onResponse(Call<CustomerData> call, Response<CustomerData> response) {
+                Log.e("TAG", "onResponse: "+response.body().toString());
+            }
+            @Override
+            public void onFailure(Call<CustomerData> call, Throwable t) {
+                Log.e("TAG", "failed: ");
+            }
+        });
+    }
+
+    public void getMerchantSetting(){
+
+
+        Call<MerchantSettingModel> merchantSettingModelCall= mzApiClient.getCustomerServices("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkhEQ0s1UkhCN0ZGSldUV1ciLCJwYXNzd29yZCI6IjlRN1UiLCJpYXQiOjE2NTIxNjA2MDQsImV4cCI6MTY1MjE4OTQwNH0.GSfwYKNMRTlg8pyIZSuts8UgmbrlDlC7cYsbo0mzpXE").getMerchantSetting();
+        merchantSettingModelCall.enqueue(new Callback<MerchantSettingModel>() {
+            @Override
+            public void onResponse(Call<MerchantSettingModel> call, Response<MerchantSettingModel> response) {
+                Log.e("TAG", "onResponse: "+response.body().toString());
+            }
+            @Override
+            public void onFailure(Call<MerchantSettingModel> call, Throwable t) {
+                Log.e("TAG", "failed: ");
+            }
+        });
+    }
+
+
+    public void getMerchantDownloadSetting(){
+        Call<MerchantDownloadSettings> merchantDownloadSettingsCall= mzApiClient.getMerchantServices("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkhEQ0s1UkhCN0ZGSldUV1ciLCJwYXNzd29yZCI6IjlRN1UiLCJpYXQiOjE2NTIyNDQ2NTQsImV4cCI6MTY1MjI3MzQ1NH0.NipLQvYhKMxD6uZjuGvrWxMuq-9ZXCiYmlJg15AloR0").getMerchantDownloadSetting();
+        merchantDownloadSettingsCall.enqueue(new Callback<MerchantDownloadSettings>() {
+            @Override
+            public void onResponse(Call<MerchantDownloadSettings> call, Response<MerchantDownloadSettings> response) {
+                Log.e("TAG", "onResponse: "+response.body().toString());
+            }
+            @Override
+            public void onFailure(Call<MerchantDownloadSettings> call, Throwable t) {
+                Log.e("TAG", "failed: ");
+            }
+        });
+    }
+
+    public void getOutletByCouponCode(){
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("limit", "20");
+        params.put("skip", "0");
+        params.put("search_by", "Aravind");
+        params.put("coupon_code","\'1004634\'");
+        Call<OutletProfileModel> outletProfileModelCall= mzApiClient.getOutletService("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkhEQ0s1UkhCN0ZGSldUV1ciLCJwYXNzd29yZCI6IjlRN1UiLCJpYXQiOjE2NTIyNDQ2NTQsImV4cCI6MTY1MjI3MzQ1NH0.NipLQvYhKMxD6uZjuGvrWxMuq-9ZXCiYmlJg15AloR0").getOutletByCouponCode(params);
+        outletProfileModelCall.enqueue(new Callback<OutletProfileModel>() {
+            @Override
+            public void onResponse(Call<OutletProfileModel> call, Response<OutletProfileModel> response) {
+                Log.e("TAG", "onResponse: "+response.body().toString());
+            }
+            @Override
+            public void onFailure(Call<OutletProfileModel> call, Throwable t) {
+                Log.e("TAG", "failed: ");
+            }
+        });
+    }
+
+
+
+    public void getOutletByRedeemPass(){
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("outlet_redeem_pass", "2222");
+
+        Call<OutletProfile> outletProfileModelCall= mzApiClient.getOutletService("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkhEQ0s1UkhCN0ZGSldUV1ciLCJwYXNzd29yZCI6IjlRN1UiLCJpYXQiOjE2NTIyNDQ2NTQsImV4cCI6MTY1MjI3MzQ1NH0.NipLQvYhKMxD6uZjuGvrWxMuq-9ZXCiYmlJg15AloR0").getOutletByRedeemPass(params);
+        outletProfileModelCall.enqueue(new Callback<OutletProfile>() {
+            @Override
+            public void onResponse(Call<OutletProfile> call, Response<OutletProfile> response) {
+                Log.e("TAG", "onResponse: "+response.body().toString());
+            }
+            @Override
+            public void onFailure(Call<OutletProfile> call, Throwable t) {
+                Log.e("TAG", "failed: ");
+            }
+        });
+    }
+
+
+
 }
